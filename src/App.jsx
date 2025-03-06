ec0b770a86414319674e91b13139072d3a6acb70
Quang030602 <minhquang030602@gmail.com>
2025-03-06 10:06:43 +0700

    FE Login , register and route for them

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Board from '~/pages/Boards/_id'
 import NotFound from '~/pages/NotFound'
+import Auth from '~/pages/Auth/Auth'
+
 
 function App() {
   return (
@@ -12,6 +14,9 @@ function App() {
       }/>
 
       <Route path='/boards/:boardId' element={ <Board /> }/>
+
+      <Route path='/login' element= {<Auth/> } />
+      <Route path='/register' element= {<Auth/> } />
 
       <Route path='*' element={<NotFound/> }/>
     </Routes>
